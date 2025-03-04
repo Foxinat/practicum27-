9871e98d7b707f2c7250ebc9be80f0b011b16660
Foxinat <vanlak21808@gmail.com>
2025-03-04 10:16:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicum27
+{
+    public class ReverseArrayReverseOnly
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива: ");
+            int size = int.Parse(Console.ReadLine());
+
+            int[] myArray = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Введите элемент массива под индексом {i}: ");
+                myArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Reverse(myArray); // Разворачиваем массив
+
+            Console.WriteLine("\nМассив в обратном порядке (Reverse):");
+            foreach (int element in myArray)
+            {
+                Console.Write(element + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
